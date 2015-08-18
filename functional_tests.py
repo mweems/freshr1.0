dc03d442172e29b76b2f9fb24420161e0e2b6969
Matt Weems <mj_weems@yahoo.com>
2015-08-18 15:00:41 +0100

    add template for homepage, adjust tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewFishermanTest(unittest.TestCase):
@@ -9,10 +10,19 @@ class NewFishermanTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
-	def test_can_add_a_post_and_retrieve_it_later(self):
+	def test_can_get_to_create_post_page_from_home_page(self):
 		self.browser.get('http://localhost:8000')
 		self.assertIn('Freshr', self.browser.title)
-		self.fail('Finish the test')
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('Freshr', header_text)
+
+		sell_button = self.browser.find_element_by_id('sell_fish')
+		buy_button = self.browser.find_element_by_id('buy_fish')
+
+		sell_button.send_keys(Keys.ENTER)
+
+		create_post_url = self.browser.current_url
+		self.assertRegex(create_post_url, '/newsFeed/create')
 
 
 if __name__== '__main__':
