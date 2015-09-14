2675dd05a9026ec5bb07001c8ff7ae41c0b1d69a
Matt Weems <mj_weems@yahoo.com>
2015-09-14 11:17:46 -1000

    update tests based on new css

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -5,6 +5,7 @@ import time
 
 class NewFishermanTest(LiveServerTestCase):
 
+
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 
@@ -14,11 +15,8 @@ class NewFishermanTest(LiveServerTestCase):
 	def test_can_get_to_create_post_page_from_home_page(self):
 		self.browser.get(self.live_server_url)
 		self.assertIn('Freshr', self.browser.title)
-		header_text = self.browser.find_element_by_tag_name('h1').text
-		self.assertIn('Freshr', header_text)
 
 		sell_button = self.browser.find_element_by_id('sell_fish')
-		buy_button = self.browser.find_element_by_id('buy_fish')
 
 		sell_button.send_keys(Keys.ENTER)
 
@@ -28,10 +26,7 @@ class NewFishermanTest(LiveServerTestCase):
 	def test_can_get_to_feed_page_from_home_page(self):
 		self.browser.get(self.live_server_url)
 		self.assertIn('Freshr', self.browser.title)
-		header_text = self.browser.find_element_by_tag_name('h1').text
-		self.assertIn('Freshr', header_text)
 
-		sell_button = self.browser.find_element_by_id('sell_fish')
 		buy_button = self.browser.find_element_by_id('buy_fish')
 
 		buy_button.send_keys(Keys.ENTER)
@@ -41,8 +36,8 @@ class NewFishermanTest(LiveServerTestCase):
 
 	def test_submitting_redirects_to_feed_page(self):
 		self.browser.get(self.live_server_url)
-		sell_button = self.browser.find_element_by_id('sell_fish').send_keys(Keys.ENTER)
-		self.assertIn('Freshr Create Post', self.browser.title)
+		self.browser.find_element_by_id('sell_fish').send_keys(Keys.ENTER)
+		time.sleep(.5)
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('Create new post', header_text)
 
@@ -63,7 +58,6 @@ class NewFishermanTest(LiveServerTestCase):
 	def test_can_create_a_post(self):
 		self.browser.get(self.live_server_url)
 		self.browser.find_element_by_id('sell_fish').send_keys(Keys.ENTER)
-		import time
 		time.sleep(.5)
 
 		nameInput = self.browser.find_element_by_id('name_input')
@@ -77,12 +71,11 @@ class NewFishermanTest(LiveServerTestCase):
 
 		submit.click()
 
-		table = self.browser.find_element_by_id('feed_table')
-		rows = table.find_elements_by_tag_name('tr')
+		post = self.browser.find_element_by_id('post')
 
-		self.assertIn('Matt', [row.text for row in rows])
-		self.assertIn('808-420-6969', [row.text for row in rows])
-		self.assertIn('10lbs Tuna, $5 per lb', [row.text for row in rows])
+		self.assertIn('Matt', post.text)
+		self.assertIn('808-420-6969', post.text)
+		self.assertIn('10lbs Tuna, $5 per lb', post.text)
 
 	def test_can_go_back_to_create_page(self):
 		self.browser.get(self.live_server_url)
@@ -134,13 +127,12 @@ class NewFishermanTest(LiveServerTestCase):
 
 		submit.click()
 
-		table = self.browser.find_element_by_id('feed_table')
-		rows = table.find_elements_by_tag_name('tr')
+		post = self.browser.find_element_by_id('post')
 
-		self.assertIn('Matt', [row.text for row in rows])
-		self.assertIn('808-420-6969', [row.text for row in rows])
-		self.assertIn('10lbs Tuna, $5 per lb', [row.text for row in rows])
+		self.assertIn('Matt', post.text)
+		self.assertIn('808-420-6969', post.text)
+		self.assertIn('10lbs Tuna, $5 per lb', post.text)
 
-		self.assertIn('Paula', [row.text for row in rows])
-		self.assertIn('808-421-6970', [row.text for row in rows])
-		self.assertIn('20lbs Ahi, $5 per lb', [row.text for row in rows])
+		self.assertIn('Paula', post.text)
+		self.assertIn('808-421-6970', post.text)
+		self.assertIn('20lbs Ahi, $5 per lb', post.text)
